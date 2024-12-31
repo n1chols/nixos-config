cb82bcbe6b69b620705f163fa1bed33488ad550d
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 15:16:25 -0800

    Create steam.nix

---
+++ modules/gaming/steam.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }: {
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    steam-hardware.enable = true;
+    xpadneo.enable = true;
+  };
+
+  users.users.user.extraGroups = [ "input" ];
+
+  # Gamescope
+
+  # MangoHUD
+
+  };
