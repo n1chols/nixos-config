f2486bf994738be1e29c51447c5303114aba2961
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-30 23:32:36 -0800

    Update audio.nix

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }: {
+
+  # RTKit
   security.rtkit.enable = true;
-  
+
+  # Pipewire
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
   };
+
 }
