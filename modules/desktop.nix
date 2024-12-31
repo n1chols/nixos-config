81e86bb14098f77aab55cb525626e8069cf5eafc
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 00:11:47 -0800

    Update desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,15 +1,16 @@
+{ config, pkgs, ... }: {
 
-# Hyprland configuration
+  # Hyprland
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
-  # Enable XDG portal with proper backends
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-    config.common.default = "*";
-  };
+  users.users.user.extraGroups = [ "video" ];
 
-users.users.user.extraGroups = [ "video" ];
+};
