699308eb16b4e16769a275020e3422f9053f6b6e
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-30 23:28:13 -0800

    Update wireless.nix

--- modules/wireless.nix
+++ modules/wireless.nix
@@ -1,8 +1,15 @@
 { config, pkgs, ... }: {
+
+  # WiFi
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
   };
 
   users.users.user.extraGroups = [ "networkmanager" ];
-}
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+};
