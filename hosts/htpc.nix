b9e1365581e27bb2101dc358e8fe069b637b6103
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 01:17:28 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -1,3 +1,26 @@
 { config, pkgs, ... }: {
-  
+
+  # Drivers
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ];
+    kernelModules = [ "kvm-amd" ];
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-opencl-runtime
+    amdvlk
+  ];
+
+  services.hardware.corectrl.enable = true;
+
+  # Optimization
+  powerManagement.cpuFreqGovernor = "performance";
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 10;
+    "vm.vfs_cache_pressure" = 50;
+  };
+
 };
