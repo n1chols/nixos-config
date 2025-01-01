7f1482b1e8027f9ed6e8d87c6fb183d2a9e44127
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 19:14:59 -0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,35 @@
 { config, pkgs, ... }: {
+
+  # Host
   imports = [
     # Replace with your host config (e.g., hosts/thinkpad.nix)
     ./hosts/example.nix
   ];
+
+  # Boot
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # Time
+  time = {
+    timeZone = "America/Los_Angeles";
+    hardwareClockInLocalTime = true;
+  };
+
+  # Localization
+  i18n.defaultLocale = "en_US.UTF-8";
+  
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  
+  # User
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
 };
