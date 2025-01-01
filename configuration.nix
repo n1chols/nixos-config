f5e3562081daaff902292994c28f78ecf7b368e9
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 19:17:23 -0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -11,20 +11,6 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
-  # Time
-  time = {
-    timeZone = "America/Los_Angeles";
-    hardwareClockInLocalTime = true;
-  };
-
-  # Localization
-  i18n.defaultLocale = "en_US.UTF-8";
-  
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
   
   # User
   users.users.user = {
