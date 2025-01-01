c5eca4eaf6bfcdef85fb2c5b7e7ec2692d412862
tob4n <109690594+tob4n@users.noreply.github.com>
2024-12-31 16:56:51 -0800

    Update steam.nix

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }: {
 
   # Steam
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
   };
   
   hardware = {
