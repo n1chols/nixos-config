aa3cbff3ba96047d1a993b4e0ff6bfcd0dac4a40
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 15:58:47 -0800

    Update steam.nix

--- modules/steam.nix
+++ modules/steam.nix
@@ -4,23 +4,12 @@
   options = {
     modules.steam = {
       enable = lib.mkEnableOption "";
-      gamepadSupport = lib.mkEnableOption "";
       addSessionEntry = lib.mkEnableOption "";
     };
   };
   
   # CONFIG
   config = lib.mkIf config.modules.steam.enable {
-    # Enable hardware support
-    hardware = {
-      graphics = {
-        enable = true;
-        enable32Bit = true;
-      };
-      steam-hardware.enable = config.modules.steam.gamepadSupport;
-      xpadneo.enable = config.modules.steam.gamepadSupport;
-    };
-
     # Enable Steam and Wayland session entry
     programs.steam = {
       enable = true;
