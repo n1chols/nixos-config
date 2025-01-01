a7d48c9ee2964d994c2f645509cba3064087326b
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 13:31:36 -0800

    Update and rename gaming.nix to steam.nix

---
+++ modules/steam.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }: {
+
+  options = {
+    modules.steam = {
+      enable = lib.mkEnableOption "Install Steam";
+      addSessionEntry = lib.mkEnableOption "Add session manager entry";
+    };
+  };
+
+  config = lib.mkIf config.modules.steam.enable {
+    # Enable graphics support
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+      steam-hardware.enable = true;
+      xpadneo.enable = true;
+    };
+    # Enable Steam and session entry
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = config.modules.steam.addSessionEntry;
+    };
+  };
+
+}
