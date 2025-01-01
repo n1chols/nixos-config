ae2e15e1448b1954f6e8f8f5f4968b3dedb08cd4
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 15:25:37 -0800

    Update and rename devtools.nix to myapps.nix

---
+++ modules/myapps.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, ... }: {
+
+  # OPTIONS
+  options = {
+    modules.myapps = {
+      enable = lib.mkEnableOption "";
+      systemApps = lib.mkEnableOption "";
+      officeApps = lib.mkEnableOption "";
+    };
+  };
+  
+  # CONFIG
+  config = lib.mkIf config.modules.myapps.enable {
+    # Add applications
+    environment.systemPackages = with pkgs; [
+      (lib.mkIf config.modules.myapps.systemApps [
+        firefox
+        thunderbird
+        zed
+        mission-center
+        blackbox
+        nemo
+        mpv
+        imv
+        zathura
+      ])
+      (lib.mkIf config.modules.myapps.officeApps [
+        libreoffice
+      ])
+    ];
+
+    # Setup file associations
+    xdg.mime.defaultApplications = {
+      (lib.mkIf config.modules.myapps.systemApps [
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "application/pdf" = "org.pwmt.zathura.desktop";
+        "application/zip" = "nemo.desktop";
+        "application/gzip" = "nemo.desktop";
+        "application/x-rar" = "nemo.desktop";
+        "application/x-7z-compressed" = "nemo.desktop";
+        "text/*" = "zed.desktop";
+        "image/*" = "imv.desktop";
+        "video/*" = "mpv.desktop";
+      ])
+      (lib.mkIf config.modules.myapps.officeApps [
+        "application/msword" = "libreoffice-writer.desktop";
+        "application/vnd.ms-excel" = "libreoffice-calc.desktop";
+        "application/vnd.ms-powerpoint" = "libreoffice-impress.desktop";
+      ])
+    };
+  };
+
+}
