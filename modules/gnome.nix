ff869a7d1451961d8df557027485d01e061e2993
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 18:01:16 -0800

    Update gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -9,34 +9,36 @@
   };
   
   # CONFIG
-  config = lib.mkIf config.modules.gnome.enable {
+  config = lib.mkMerge [
     # Enable GNOME and GDM
-    services.xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      displayManager.gdm.wayland = true;
-      desktopManager.gnome.enable = true;
-    };
-
-    # Enable XDG desktop portal
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
-    };
-
-  # Disable GNOME apps
-  } // lib.optionalAttrs config.modules.gnome.disableCoreApps {
-    services = {
-      gnome = {
-        core-utilities.enable = false;
-        gnome-initial-setup.enable = false;
+    (lib.mkIf config.modules.gnome.enable {
+      services.xserver = {
+        enable = true;
+        displayManager.gdm.enable = true;
+        displayManager.gdm.wayland = true;
+        desktopManager.gnome.enable = true;
       };
-    };
 
-    environment.gnome.excludePackages = (with pkgs.gnome; [
-      gnome-tour
-      gnome-shell-extensions
-    ]);
-  };
+      # Enable XDG desktop portal
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
+      };
+    })
+
+    # Disable GNOME Apps
+    (lib.mkIf config.modules.gnome.disableCoreApps {
+      services = {
+        gnome = {
+          core-utilities.enable = false;
+          gnome-initial-setup.enable = false;
+        };
+      };
+      environment.gnome.excludePackages = (with pkgs.gnome; [
+        gnome-tour
+        gnome-shell-extensions
+      ]);
+    })
+  ];
 
 }
