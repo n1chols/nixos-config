fde58e07b414b3540ce5261d7a17021228b8c4f6
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-02 01:15:42 -0800

    Update kodi.nix

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -12,13 +12,14 @@
   config = lib.mkIf config.modules.kodi.enable {
     # Add Kodi package
     environment.systemPackages = [ pkgs.kodi ];
-    
-    # Add Wayland session entry
-    services.xserver.desktopManager.kodi.enable = config.modules.kodi.addSessionEntry;
-    
+
     # Enable required services
     services.udisks2.enable = true;
+
     xdg.portal.enable = true;
+
+    # Add Wayland session entry
+    services.xserver.desktopManager.kodi.enable = config.modules.kodi.addSessionEntry;
   };
 
 }
