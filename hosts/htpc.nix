e9edd1e6fd6e90eee0d8e51f3b5ca393b5441ea4
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 16:35:04 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -44,18 +44,20 @@
   ];
 
   services = {
-    gnome.enable = true;
-    myapps = {
-      enable = true;
-      systemApps = true;
-    };
-    steam = {
-      enable = true;
-      addSessionEntry = true;
-    };
-    kodi = {
-      enable = true;
-      addSessionEntry = true;
+    modules = {
+      gnome.enable = true;
+      myapps = {
+        enable = true;
+        systemApps = true;
+      };
+      steam = {
+        enable = true;
+        addSessionEntry = true;
+      };
+      kodi = {
+        enable = true;
+        addSessionEntry = true;
+      };
     };
   };
 
