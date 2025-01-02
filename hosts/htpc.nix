9fdb8d2c3e716b043eac2de2218a1c14c6fbfdb5
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-01 16:19:32 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -25,7 +25,108 @@
       device = "/dev/nvme0n1p2";
       fsType = "ext4";
     };
+    Is this the correct way to import modules?
+
+
+{ config, pkgs, ... }: {
+
+  # Host name
+
+  networking.hostName = "BEDROOM_HTPC";
+
+  
+
+  # Hardware setup
+
+  hardware = {
+
+    enableRedistributableFirmware = true;
+
+    cpu.amd.updateMicrocode = true;
+
+    graphics = {
+
+      enable = true;
+
+      enable32Bit = true;
+
+    };
+
+    steam-hardware.enable = true;
+
+    xpadneo.enable = true;
+
+  };
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # File systems
+
+  fileSystems = {
+
+    "/" = {
+
+      device = "/dev/nvme0n1p2";
+
+      fsType = "ext4";
+
+    };
+
     
+
+    "/boot" = {
+
+      device = "/dev/nvme0n1p1";
+
+      fsType = "vfat";
+
+    };
+
+  };
+
+  swapDevices = [
+
+    { device = "/dev/nvme0n1p3"; }
+
+  ];
+
+  # Modules configuration
+
+  imports = [ ../modules/* ];
+
+  services = {
+
+    gnome.enable = true;
+
+    myapps = {
+
+      enable = true;
+
+      systemApps = true;
+
+    };
+
+    steam = {
+
+      enable = true;
+
+      addSessionEntry = true;
+
+    };
+
+    kodi = {
+
+      enable = true;
+
+      addSessionEntry = true;
+
+    };
+
+  };
+
+}
     "/boot" = {
       device = "/dev/nvme0n1p1";
       fsType = "vfat";
@@ -37,7 +138,12 @@
   ];
 
   # Modules configuration
-  imports = [ ../modules/* ];
+  imports = [
+    ./modules/gnome.nix
+    ./modules/myapps.nix
+    ./modules/steam.nix
+    ./modules/kodi.nix
+  ];
 
   services = {
     gnome.enable = true;
