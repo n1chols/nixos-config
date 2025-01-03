dfb25bd7503d6946373c41b0ff99304d342c4968
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-02 20:59:38 -0800

    Update greetd.nix

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -3,13 +3,11 @@
   # OPTIONS
   options.modules.greetd = {
     enable = lib.mkEnableOption "";
-    sessions = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.submodule {
-        options = {
-          command = lib.mkOption { type = lib.types.str; };
-          tty = lib.mkOption { type = lib.types.int; };
-        };
-      });
+    defaultSession = lib.mkOption {
+      type = lib.types.str;
+    };
+    otherSessions = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
     };
   };
 
