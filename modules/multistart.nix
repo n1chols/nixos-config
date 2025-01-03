0f0b0c077630e9117c060e8b414d6a49df0afaaa
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 12:42:09 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -11,102 +11,27 @@
   # CONFIG
   config = lib.mkIf config.modules.multistart.enable {
 
-    systemd.services = {
-      gnome-session-tty1 = {
-        description = "GNOME Session on TTY1";
-        after = [ "systemd-user-sessions.service" "network.target" "sound.target" ];
-        requires = [ "systemd-logind.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "simple";
-          User = "user";  # Replace with your username
-          WorkingDirectory = "~";
-          PAMName = "login";
-          TTYPath = "/dev/tty1";
-          StandardInput = "tty";
-          StandardOutput = "tty";
-          StandardError = "journal";
-          UtmpIdentifier = "tty1";
-          UtmpMode = "user";
-          RuntimeDirectory = "gnome-session";
-          RuntimeDirectoryMode = "0700";
-          ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
-          ExecStart = ''
-            ${pkgs.bash}/bin/bash -l -c "dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session"
-          '';
-        };
-        environment = {
-          XDG_SESSION_TYPE = "wayland";
-          DISPLAY = ":0";
-          HOME = "/home/user";  # Replace with your username
-          USER = "user";  # Replace with your username
-          SHELL = "/run/current-system/sw/bin/bash";
-        };
-      };
-
-      steam-gamescope-tty2 = {
-        description = "Steam Big Picture (Gamescope) on TTY2";
-        after = [ "systemd-user-sessions.service" "network.target" "sound.target" ];
-        requires = [ "systemd-logind.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "simple";
-          User = "user";  # Replace with your username
-          WorkingDirectory = "~";
-          PAMName = "login";
-          TTYPath = "/dev/tty2";
-          StandardInput = "tty";
-          StandardOutput = "tty";
-          StandardError = "journal";
-          UtmpIdentifier = "tty2";
-          UtmpMode = "user";
-          RuntimeDirectory = "steam";
-          RuntimeDirectoryMode = "0700";
-          ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
-          ExecStart = ''
-            ${pkgs.bash}/bin/bash -l -c "${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
-          '';
-        };
-        environment = {
-          DISPLAY = ":1";
-          HOME = "/home/user";
-          USER = "user";
-          SHELL = "/run/current-system/sw/bin/bash";
-        };
-      };
-
-      kodi-tty3 = {
-        description = "Kodi on TTY3";
-        after = [ "systemd-user-sessions.service" "network.target" "sound.target" ];
-        requires = [ "systemd-logind.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "simple";
-          User = "user";  # Replace with your username
-          WorkingDirectory = "~";
-          PAMName = "login";
-          TTYPath = "/dev/tty3";
-          StandardInput = "tty";
-          StandardOutput = "tty";
-          StandardError = "journal";
-          UtmpIdentifier = "tty3";
-          UtmpMode = "user";
-          RuntimeDirectory = "kodi";
-          RuntimeDirectoryMode = "0700";
-          ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
-          ExecStart = ''
-            ${pkgs.bash}/bin/bash -l -c "env LIRC_SOCKET_PATH=/run/lirc/lircd ${pkgs.kodi}/bin/kodi --standalone"
-          '';
-        };
-        environment = {
-          DISPLAY = ":2";
-          HOME = "/home/user";  # Replace with your username
-          USER = "user";  # Replace with your username
-          SHELL = "/run/current-system/sw/bin/bash";
-        };
-      };
-    };
-
+    services.getty.autologinUser = "user";  # Replace with your username
+  
+    environment.etc."profile.d/auto-start-sessions.sh".text = ''
+      case "$(tty)" in
+        /dev/tty1)
+          if [[ ! $DISPLAY ]]; then
+            exec dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
+          fi
+          ;;
+        /dev/tty2)
+          if [[ ! $DISPLAY ]]; then
+            exec ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
+          fi
+          ;;
+        /dev/tty3)
+          if [[ ! $DISPLAY ]]; then
+            exec ${pkgs.kodi}/bin/kodi --standalone
+          fi
+          ;;
+      esac
+    '';
 
     # Enable required services
     services.xserver.enable = true;
