270555b5712d5a57013c978e091d630df17fc448
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 14:32:14 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -11,13 +11,13 @@
   # CONFIG
   config = lib.mkIf config.modules.multistart.enable {
 
-    services.greetd = {
-      enable = true;
-      settings.default_session = {
-        command = "dbus-run-session env XDG_RUNTIME_DIR=/run/user/$(id -u) ${pkgs.kodi}/bin/kodi --standalone";
-        user = "user";
-      };
-    };
+    #services.greetd = {
+    #  enable = true;
+    #  settings.default_session = {
+    #    command = "dbus-run-session env XDG_RUNTIME_DIR=/run/user/$(id -u) ${pkgs.kodi}/bin/kodi --standalone";
+    #    user = "user";
+    #  };
+    #};
   
     # dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
     # ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
