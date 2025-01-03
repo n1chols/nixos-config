bb36e90b77de032447142b0962035b40d01f1d20
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 12:23:51 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -11,8 +11,77 @@
   # CONFIG
   config = lib.mkIf config.modules.multistart.enable {
 
-    
+    systemd.services = {
+      gnome-session-tty1 = {
+        description = "GNOME Session on TTY1";
+        after = [ "systemd-user-sessions.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "simple";
+          User = "YOUR_USERNAME";  # Replace with your username
+          PAMName = "login";
+          TTYPath = "/dev/tty1";
+          StandardInput = "tty";
+          StandardOutput = "tty";
+          StandardError = "journal";
+          UtmpIdentifier = "tty1";
+          UtmpMode = "user";
+          ExecStart = "${pkgs.runtimeShell} -c 'dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session'";
+        };
+        environment = {
+          XDG_SESSION_TYPE = "wayland";
+          DISPLAY = ":0";
+        };
+      };
 
+      steam-gamescope-tty2 = {
+        description = "Steam Big Picture (Gamescope) on TTY2";
+        after = [ "systemd-user-sessions.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "simple";
+          User = "YOUR_USERNAME";  # Replace with your username
+          PAMName = "login";
+          TTYPath = "/dev/tty2";
+          StandardInput = "tty";
+          StandardOutput = "tty";
+          StandardError = "journal";
+          UtmpIdentifier = "tty2";
+          UtmpMode = "user";
+          ExecStart = "${pkgs.runtimeShell} -c '${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf'";
+        };
+        environment = {
+          DISPLAY = ":1";
+        };
+      };
+
+      kodi-tty3 = {
+        description = "Kodi on TTY3";
+        after = [ "systemd-user-sessions.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "simple";
+          User = "YOUR_USERNAME";  # Replace with your username
+          PAMName = "login";
+          TTYPath = "/dev/tty3";
+          StandardInput = "tty";
+          StandardOutput = "tty";
+          StandardError = "journal";
+          UtmpIdentifier = "tty3";
+          UtmpMode = "user";
+          ExecStart = "${pkgs.runtimeShell} -c 'env LIRC_SOCKET_PATH=/run/lirc/lircd ${pkgs.kodi}/bin/kodi --standalone'";
+        };
+        environment = {
+          DISPLAY = ":2";
+        };
+      };
+    };
+
+
+    # Enable required services
+    services.xserver.enable = true;
+    services.dbus.enable = true;
+    hservices.lirc.enable = true;
   };
 
 }
