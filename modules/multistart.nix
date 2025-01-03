fb79208df3b11f76452e8ad3c211e00e664bdbba
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 12:53:10 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -13,35 +13,53 @@
 
     services.greetd = {
       enable = true;
+      vt = 1;  # Start on tty1
       settings = {
         default_session = {
-          command = "./etc/greetd/default-session";
+          command = "dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session";
           user = "user";
         };
       };
     };
+  
+    # Additional greetd instances for Steam and Kodi
+    systemd.services."greetd-tty2" = {
+      description = "Steam Big Picture Mode on TTY 2";
+      after = [ "systemd-user-sessions.service" ];
+      wantedBy = [ "multi-user.target" ];
 
-    # Create the script that launches our sessions
-    environment.etc."greetd/default-session".text = ''
-      #!${pkgs.bash}/bin/bash
-    
-      case $(tty) in
-        "/dev/tty1")
-          exec dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
-          ;;
-        "/dev/tty2")
-          exec ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
-          ;;
-        "/dev/tty3")
-          exec ${pkgs.kodi}/bin/kodi --standalone
-          ;;
-      esac
-    '';
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.greetd.greetd}/bin/greetd \
+            --config <(echo '{ "terminal": { "vt": 2 }, \
+            "default_session": { \
+              "command": "${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf", \
+              "user": "user" \
+            } }')
+        '';
+        Restart = "always";
+      };
+    };
 
-    # Make the script executable
-    system.activationScripts.greetdScript = ''
-      chmod +x /etc/greetd/default-session
-    '';
+    systemd.services."greetd-tty3" = {
+      description = "Kodi on TTY 3";
+      after = [ "systemd-user-sessions.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.greetd.greetd}/bin/greetd \
+            --config <(echo '{ "terminal": { "vt": 3 }, \
+            "default_session": { \
+              "command": "${pkgs.kodi}/bin/kodi --standalone", \
+              "user": "user" \
+            } }')
+        '';
+        Restart = "always";
+      };
+    };
 
     # Enable autologin on TTYs 1-3
     services.getty.autologinUser = "user";
