1d1b2bdd383ca22e1ef95c7b947d5cfdc401d11d
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 12:48:24 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -11,27 +11,46 @@
   # CONFIG
   config = lib.mkIf config.modules.multistart.enable {
 
-    services.getty.autologinUser = "user";  # Replace with your username
-  
-    environment.etc."profile.d/auto-start-sessions.sh".text = ''
-      case "$(tty)" in
-        /dev/tty1)
-          if [[ ! $DISPLAY ]]; then
-            exec dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
-          fi
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "./etc/greetd/default-session";
+          user = "user";
+        };
+      };
+    };
+
+    # Create the script that launches our sessions
+    environment.etc."greetd/default-session".text = ''
+      #!${pkgs.bash}/bin/bash
+    
+      case $(tty) in
+        "/dev/tty1")
+          exec dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
           ;;
-        /dev/tty2)
-          if [[ ! $DISPLAY ]]; then
-            exec ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
-          fi
+        "/dev/tty2")
+          exec ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
           ;;
-        /dev/tty3)
-          if [[ ! $DISPLAY ]]; then
-            exec ${pkgs.kodi}/bin/kodi --standalone
-          fi
+        "/dev/tty3")
+          exec ${pkgs.kodi}/bin/kodi --standalone
           ;;
       esac
     '';
+
+    # Make the script executable
+    system.activationScripts.greetdScript = ''
+      chmod +x /etc/greetd/default-session
+    '';
+
+    # Enable autologin on TTYs 1-3
+    services.getty.autologinUser = "user";
+    #services.getty.extraArgs = [ "--noclear" ];  # Prevent screen clearing
+    services.getty.greetingLine = "";  # Remove login message
+
+    # Enable TTYs 1-3
+    services.getty.helpLine = "";
+    services.getty.terminals = [ "tty1" "tty2" "tty3" ];
 
     # Enable required services
     services.xserver.enable = true;
