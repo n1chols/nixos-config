40f5edd7811051e4c83f397ecdfc8a5f3721fc60
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 00:23:33 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -13,9 +13,22 @@
     # Enable autologin
     services.getty.autologinUser = "user";
 
-    # Start session(s)
-
-    
+    # Create systemd services for each session
+    systemd.services = builtins.listToAttrs (
+      lib.imap0 (index: session: {
+        name = "session-tty${toString (index + 1)}";
+        value = {
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "simple";
+            User = "user";
+            TTYPath = "/dev/tty${toString (index + 1)}";
+            ExecStart = "${pkgs.bash}/bin/bash -l -c '${session}'";
+            Restart = "always";
+          };
+        };
+      }) config.modules.multistart.sessions
+    );
   };
 
 }
