c7190d35a6314a56f3ad22e63d56494c4a68d4d5
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-02 19:14:37 -0800

    Update autotty.nix

--- modules/autotty.nix
+++ modules/autotty.nix
@@ -1,42 +1,39 @@
 { config, lib, pkgs, ... }: {
 
-
   options = {
     modules.autotty = {
       enable = lib.mkEnableOption "";
+      services = lib.mkOption {
+        default = {};
+        type = with lib.types; attrsOf (submodule {
+          options = {
+            tty = lib.mkOption {
+              type = str;
+            };
+            command = lib.mkOption {
+              type = str;
+            };
+          };
+        });
+      };
     };
   };
 
   config = lib.mkIf config.modules.autotty.enable {
+    # Disable display manager
     services.xserver.displayManager.enabled = false;
-    
+
+    # Enable tty auto login
     services.getty.autologinUser = "user";
 
-    systemd.user.services = {
-      tty1-gnome = {
-        wantedBy = [ "default.target" ];
-        serviceConfig = {
-          ExecStart = "${pkgs.gnome.gnome-session}/bin/gnome-session";
-          TTYPath = "/dev/tty1";
-        };
+    # Create login service(s)
+    systemd.user.services = lib.mapAttrs (name: service: {
+      wantedBy = [ "default.target" ];
+      serviceConfig = {
+        ExecStart = service.command;
+        TTYPath = "/dev/${service.tty}";
       };
-
-      tty2-steam = {
-        wantedBy = [ "default.target" ];
-        serviceConfig = {
-          ExecStart = "${pkgs.gamescope}/bin/gamescope -f -- ${pkgs.steam}/bin/steam -bigpicture";
-          TTYPath = "/dev/tty2";
-        };
-      };
-
-      tty3-kodi = {
-        wantedBy = [ "default.target" ];
-        serviceConfig = {
-          ExecStart = "${pkgs.kodi}/bin/kodi-standalone";
-          TTYPath = "/dev/tty3";
-        };
-      };
-    };
+    }) config.modules.autotty.services;
   };
 
 }
