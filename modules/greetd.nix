20d29b0eb7d7c4cc16d625f7f54b4f32d0c88253
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-02 20:54:44 -0800

    Update greetd.nix

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,56 +1,15 @@
 { config, lib, pkgs, ... }: {
 
-  options = {
-    modules.greetd = {
-      enable = lib.mkEnableOption "";
-      sessions = lib.mkOption {
-        default = {};
-        type = with lib.types; attrsOf str;
-      };
+  options.modules.greetd = {
+    enable = lib.mkEnableOption "";
+    sessions = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule {
+        options = {
+          command = lib.mkOption { type = lib.types.str; };
+          tty = lib.mkOption { type = lib.types.int; };
+        };
+      });
     };
   };
-
-  config = lib.mkIf config.modules.greetd.enable {
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = builtins.elemAt (lib.attrValues config.modules.greetd.sessions) 0;
-          user = "user";
-        };
-      };
-    };
-
-    environment.etc = lib.listToAttrs (lib.imap1 (index: name: {
-      name = "greetd/${name}-config.toml";
-      value = {
-        text = ''
-          [terminal]
-          vt = ${toString (index + 1)}
-
-          [default_session]
-          command = "${lib.getAttr name config.modules.greetd.sessions}"
-          user = "user"
-        '';
-      };
-    }) (lib.tail (lib.attrNames config.modules.greetd.sessions)));
-
-    # For Kodi's gvfs access
-    security.pam.services.greetd.enableGnomeKeyring = true;
-    services.gvfs.enable = true;
-
-    systemd.services = lib.listToAttrs (map (name: {
-      name = "greetd-${name}";
-      value = {
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.greetd.greetd}/bin/greetd --config /etc/greetd/${name}-config.toml";
-          Restart = "always";
-        };
-      };
-    }) (lib.tail (lib.attrNames config.modules.greetd.sessions)));
-  };
-
 
 }
