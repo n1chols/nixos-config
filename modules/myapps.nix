a4701fc30763c9c2f4b484944019e6ce02b70382
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-02 18:06:15 -0800

    Update myapps.nix

--- modules/myapps.nix
+++ modules/myapps.nix
@@ -22,7 +22,7 @@
         mission-center
         blackbox-terminal
         git
-        find
+        findutils
       ])) ++
       (lib.optionals config.modules.myapps.officeApps (with pkgs; [
         libreoffice
