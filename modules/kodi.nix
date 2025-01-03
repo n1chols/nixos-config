69f1478d50d0c5289ed5a8e96d61c10060e0571f
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 14:31:50 -0800

    Update kodi.nix

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -10,22 +10,10 @@
   # CONFIG
   config = lib.mkIf config.modules.kodi.enable {
 
-    services.xserver.desktopManager.kodi.enable = true;
-
-    security.rtkit.enable = true;
-    services.pipewire.enable = true;
-    services.pipewire.pulse.enable = true;
-    services.xserver.enable = true;
-
-    environment.systemPackages = [ pkgs.libcec ];
-
-    users.users.kodi = {
-      isNormalUser = true;
-      extraGroups = [ "input" "video" "audio"];
-    };
-
-    services.xserver.displayManager.autoLogin.enable = true;
-    services.xserver.displayManager.autoLogin.user = "kodi";
+    users.extraUsers.kodi.isNormalUser = true;
+    services.cage.user = "kodi";
+    services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+    services.cage.enable = true;
     
   };
 
