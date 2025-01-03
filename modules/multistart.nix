8959fb4faf2ff7dea07219b2598443f31d6fd158
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 13:26:00 -0800

    Update multistart.nix

--- modules/multistart.nix
+++ modules/multistart.nix
@@ -13,60 +13,16 @@
 
     services.greetd = {
       enable = true;
-      vt = 1;  # Start on tty1
-      settings = {
-        default_session = {
-          command = "${pkgs.kodi}/bin/kodi --standalone & waitPID=$!";#"dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session";
-          user = "user";
-        };
+      settings.default_session = {
+        command = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+        user = "user";
       };
     };
   
-    # Additional greetd instances for Steam and Kodi
-    systemd.services."greetd-tty2" = {
-      description = "Steam Big Picture Mode on TTY 2";
-      after = [ "systemd-user-sessions.service" ];
-      wantedBy = [ "multi-user.target" ];
+    # dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session
+    # ${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf
+    # ${pkgs.kodi-wayland}/bin/kodi-standalone
 
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''
-          ${pkgs.greetd.greetd}/bin/greetd \
-            --config <(echo '{ "terminal": { "vt": 2 }, \
-            "default_session": { \
-              "command": "${pkgs.gamescope}/bin/gamescope -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf", \
-              "user": "user" \
-            } }')
-        '';
-        Restart = "always";
-      };
-    };
-
-    systemd.services."greetd-tty3" = {
-      description = "Kodi on TTY 3";
-      after = [ "systemd-user-sessions.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''
-          ${pkgs.greetd.greetd}/bin/greetd \
-            --config <(echo '{ "terminal": { "vt": 3 }, \
-            "default_session": { \
-              "command": "${pkgs.kodi}/bin/kodi --standalone", \
-              "user": "user" \
-            } }')
-        '';
-        Restart = "always";
-      };
-    };
-
-    # Enable autologin on TTYs 1-3
-    services.getty.autologinUser = "user";
-
-    # Enable required services
-    services.xserver.enable = true;
-    services.dbus.enable = true;
   };
 
 }
