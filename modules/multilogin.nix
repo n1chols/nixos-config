70faccd7428673aa1618fb4be38d4d91f40f0c68
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 16:37:59 -0800

    Update multilogin.nix

--- modules/multilogin.nix
+++ modules/multilogin.nix
@@ -9,37 +9,30 @@
   };
 
   config = lib.mkIf config.modules.multilogin.enable {
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = lib.head config.modules.multilogin.sessions;
-          user = "user";
-        };
-      };
+    services.greetd.enable = true;
+    services.greetd.settings.default_session = {
+      command = lib.head config.modules.multilogin.sessions;
+      user = "user";
     };
 
-    environment.etc = lib.listToAttrs (lib.imap1 (index: command: {
-      name = "greetd/session${toString index}-config.toml";
-      value = {
-        text = ''
-          [terminal]
-          vt = ${toString (index + 1)}
-
-          [default_session]
-          command = "${command}"
-          user = "user"
-        '';
-      };
+    environment.etc = lib.listToAttrs (lib.imap1 (i: cmd: {
+      name = "greetd/session${toString i}-config.toml";
+      value.text = ''
+        [terminal]
+        vt = ${toString (i + 1)}
+        [default_session]
+        command = "${cmd}"
+        user = "user"
+      '';
     }) (lib.tail config.modules.multilogin.sessions));
 
-    systemd.services = lib.listToAttrs (lib.imap1 (index: command: {
-      name = "greetd-session${toString index}";
+    systemd.services = lib.listToAttrs (lib.imap1 (i: cmd: {
+      name = "greetd-session${toString i}";
       value = {
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           Type = "simple";
-          ExecStart = "${pkgs.greetd.greetd}/bin/greetd --config /etc/greetd/session${toString index}-config.toml";
+          ExecStart = "${pkgs.greetd.greetd}/bin/greetd --config /etc/greetd/session${toString i}-config.toml";
           Restart = "always";
         };
       };
