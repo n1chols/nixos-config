f0d8e4a1822ebedf4330d8b46f1890edc9432d28
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 19:22:22 -0800

    Update kodi.nix

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -11,19 +11,8 @@
   config = lib.mkIf config.modules.kodi.enable {
     # Add Kodi package, compositor, and addons
     environment.systemPackages = with pkgs; [
+      kodi
       cage
-      kodi.override {
-        extraAddons = {
-          repository-umbrella = pkgs.fetchzip {
-            url = "https://umbrellaplug.github.io/repository.umbrella-2.2.6.zip";
-            sha256 = "sha256-XXXXX";
-          };
-          repository-cocoscrapers = pkgs.fetchzip {
-            url = "https://cocojoe2411.github.io/repository.cocoscrapers-1.0.0.zip";
-            sha256 = "sha256-XXXXX";
-          };
-        };
-      }
     ];
   };
 
