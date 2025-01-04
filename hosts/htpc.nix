e9438698e056d905580785ac192a8ffb3de83cfb
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-03 17:04:46 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -59,7 +59,7 @@
       sessions = [
         "dbus-run-session env XDG_SESSION_TYPE=wayland gnome-session"
         "${pkgs.gamescope}/bin/gamescope --steam -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
-        "${pkgs.cage}/bin/cage -- ${pkgs.kodi-wayland}/bin/kodi --windowing=wayland"
+        "${pkgs.sway}/bin/sway -config /dev/null -c 'exec ${pkgs.kodi-wayland}/bin/kodi --windowing=wayland'"
       ];
     };
   };
