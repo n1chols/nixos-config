773745e0ea87a2c7b1c5fdc816bb0aa18b651385
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-05 04:10:02 -0800

    Update and rename myapps.nix to myapps.nix

---
+++ bundles/myapps.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }: {
+  
+  # Add applications
+  environment.systemPackages = [
+    firefox
+    thunderbird
+    loupe
+    celluloid
+    nautilus
+    mission-center
+    blackbox-terminal
+    git
+    cage
+  ];
+
+  # Setup file associations
+  xdg.mime.defaultApplications = {
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "application/pdf" = "firefox.desktop";
+    "application/zip" = "nautilus.desktop";
+    "application/gzip" = "nautilus.desktop";
+    "application/x-rar" = "nautilus.desktop";
+    "application/x-7z-compressed" = "nautilus.desktop";
+    "text/*" = "zed.desktop";
+    "image/*" = "loupe.desktop";
+    "video/*" = "celluloid.desktop";
+  };
+
+}
