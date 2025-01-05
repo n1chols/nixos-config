a1a591aa953664e729e3c550723d061b22a54d21
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-05 03:38:28 -0800

    Update kodi.nix

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -1,18 +1,15 @@
 { config, lib, pkgs, ... }: {
 
   # OPTIONS
-  options = {
-    modules.kodi = {
-      enable = lib.mkEnableOption "";
-    };
+  options.modules.kodi = {
+    enable = lib.mkEnableOption "";
   };
   
   # CONFIG
   config = lib.mkIf config.modules.kodi.enable {
-    # Add Kodi package and compositor
+    # Add Kodi package
     environment.systemPackages = with pkgs; [
       kodi
-      cage
     ];
   };
 
