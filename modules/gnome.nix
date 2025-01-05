73402d636f36e14babb143e0a579a73a90b661c4
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-05 03:38:48 -0800

    Update gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,10 +1,8 @@
 { config, lib, pkgs, ... }: {
 
   # OPTIONS
-  options = {
-    modules.gnome = {
-      enable = lib.mkEnableOption "";
-    };
+  options.modules.gnome = {
+    enable = lib.mkEnableOption "";
   };
   
   # CONFIG
