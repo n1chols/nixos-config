385f1095ae927c4acf5f9b849f633c0dc3e62629
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-05 04:18:04 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }: {
 
-  # Host name
+  # HOST NAME / TIME ZONE
   networking.hostName = "HTPC";
-
-  # Time zone
   time.timeZone = "America/Los_Angeles";
   
-  # Hardware setup
+  # HARDWARE / DRIVERS
   hardware = {
     enableRedistributableFirmware = true;
     cpu.amd.updateMicrocode = true;
@@ -17,11 +15,10 @@
     xpadneo.enable = true;
   };
 
+  boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  # File systems
+  # FILE SYSTEM
   fileSystems = {
     "/" = {
       device = "/dev/nvme0n1p2";
@@ -37,31 +34,30 @@
     { device = "/dev/nvme0n1p3"; }
   ];
 
-  # Modules configuration
+  # PACKAGES
+  environment.systemPackages = [
+    cage
+  ];
+
+  # IMPORTS
   imports = [
-    ../modules/gnome.nix
-    ../modules/steam.nix
-    ../modules/es-de.nix
-    ../modules/kodi.nix
-    ../modules/myapps.nix
+    ../bundles/gnome.nix
+    ../bundles/steam.nix
+    ../bundles/es-de.nix
+    ../bundles/kodi.nix
+    ../bundles/myapps.nix
     ../modules/multilogin.nix
   ];
 
-  modules = {
-    gnome.enable = true;
-    steam.enable = true;
-    es-de.enable = true;
-    kodi.enable = true;
-    myapps.enable = true;
-    multilogin = {
-      enable = true;
-      sessions = [
-        "XDG_SESSION_TYPE=wayland ${pkgs.gnome.gnome-session}/bin/gnome-session"
-        "${pkgs.gamescope}/bin/gamescope --steam -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
-        "${pkgs.cage}/bin/cage -s ${pkgs.emulationstation-de}/bin/es-de"
-        "${pkgs.cage}/bin/cage -s ${pkgs.kodi}/bin/kodi-standalone"
-      ];
-    };
+  # MODULES
+  modules.multilogin = {
+    enable = true;
+    sessions = [
+      "XDG_SESSION_TYPE=wayland ${pkgs.gnome.gnome-session}/bin/gnome-session"
+      "${pkgs.gamescope}/bin/gamescope --steam -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
+      "${pkgs.cage}/bin/cage -s ${pkgs.emulationstation-de}/bin/es-de"
+      "${pkgs.cage}/bin/cage -s ${pkgs.kodi}/bin/kodi-standalone"
+    ];
   };
 
 }
