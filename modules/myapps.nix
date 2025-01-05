3b5c7fe05e5b6e5d2fdc3f808c158f1950f1bb80
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-05 03:36:28 -0800

    Update myapps.nix

--- modules/myapps.nix
+++ modules/myapps.nix
@@ -1,52 +1,38 @@
 { config, lib, pkgs, ... }: {
 
   # OPTIONS
-  options = {
-    modules.myapps = {
-      enable = lib.mkEnableOption "";
-      systemApps = lib.mkEnableOption "";
-      officeApps = lib.mkEnableOption "";
-    };
+  options.modules.myapps = {
+    enable = lib.mkEnableOption "";
   };
   
   # CONFIG
   config = lib.mkIf config.modules.myapps.enable {
     # Add applications
     environment.systemPackages = 
-      (lib.optionals config.modules.myapps.systemApps (with pkgs; [
-        firefox
-        thunderbird
-        loupe
-        celluloid
-        nautilus
-        mission-center
-        blackbox-terminal
-        git
-        findutils
-      ])) ++
-      (lib.optionals config.modules.myapps.officeApps (with pkgs; [
-        libreoffice
-      ]));
+      firefox
+      thunderbird
+      loupe
+      celluloid
+      nautilus
+      mission-center
+      blackbox-terminal
+      git
+      cage
+    ];
 
     # Setup file associations
-    xdg.mime.defaultApplications =
-      (lib.optionalAttrs config.modules.myapps.systemApps {
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "application/pdf" = "firefox.desktop";
-        "application/zip" = "nautilus.desktop";
-        "application/gzip" = "nautilus.desktop";
-        "application/x-rar" = "nautilus.desktop";
-        "application/x-7z-compressed" = "nautilus.desktop";
-        "text/*" = "zed.desktop";
-        "image/*" = "loupe.desktop";
-        "video/*" = "celluloid.desktop";
-      }) //
-      (lib.optionalAttrs config.modules.myapps.officeApps {
-        "application/msword" = "libreoffice-writer.desktop";
-        "application/vnd.ms-excel" = "libreoffice-calc.desktop";
-        "application/vnd.ms-powerpoint" = "libreoffice-impress.desktop";
-      });
+    xdg.mime.defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "application/pdf" = "firefox.desktop";
+      "application/zip" = "nautilus.desktop";
+      "application/gzip" = "nautilus.desktop";
+      "application/x-rar" = "nautilus.desktop";
+      "application/x-7z-compressed" = "nautilus.desktop";
+      "text/*" = "zed.desktop";
+      "image/*" = "loupe.desktop";
+      "video/*" = "celluloid.desktop";
+    };
   };
 
 }
