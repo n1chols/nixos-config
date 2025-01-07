10729066c63d9bdc7c245941d9ebc68351a0d023
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-07 08:37:09 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -47,9 +47,11 @@
   modules.multilogin = {
     enable = true;
     sessions = [
-      "${pkgs.gamescope}/bin/gamescope -w 3840 -h 2160 -r 120 \
+      ''
+        ${pkgs.gamescope}/bin/gamescope -w 3840 -h 2160 -r 120 \
         --backend drm --immediate-flips --rt --fullscreen --adaptive-sync --hdr-enabled -- \
-        sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
+        sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'
+      ''
       "${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.gnome-session}/bin/gnome-session"
     ];
   };
