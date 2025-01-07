9dc7705546ce5b64f80825439013d38d5cccb8c7
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-07 02:22:57 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -54,8 +54,9 @@
     enable = true;
     sessions = [
       "XDG_SESSION_TYPE=wayland ${pkgs.gnome-session}/bin/gnome-session"
-      "${pkgs.gamescope}/bin/gamescope --backend drm --immediate-flips --rt --steam -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
-      "${pkgs.cage}/bin/cage -s ${pkgs.pegasus-frontend}/bin/pegasus-fe"
+      #"${pkgs.gamescope}/bin/gamescope --backend drm --immediate-flips --rt --steam -- ${pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf"
+      #"${pkgs.cage}/bin/cage -s ${pkgs.pegasus-frontend}/bin/pegasus-fe"
+      "${pkgs.gamescope}/bin/gamescope --backend drm --immediate-flips --rt -- ${pkgs.pegasus-frontend}/bin/pegasus-fe"
       "${pkgs.cage}/bin/cage -s ${pkgs.kodi}/bin/kodi-standalone"
     ];
   };
