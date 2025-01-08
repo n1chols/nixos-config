3ac961dbe41820ca4b1bfb2530e13c78970f4e55
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-07 19:06:15 -0800

    Update bombsquad.nix

--- apps/bombsquad.nix
+++ apps/bombsquad.nix
@@ -1,17 +1,14 @@
 { config, lib, pkgs, ... }: {
 
-  # Install BombSquad
+  # Install BombSquad with hash override
   environment.systemPackages = with pkgs; [
-    bombsquad
+    (stdenv.mkDerivation rec {
+      name = "bombsquad";
+      src = fetchurl {
+        url = "https://files.ballistica.net/bombsquad/builds/BombSquad_Linux_x86_64_1.7.37.tar.gz";
+        sha256 = lib.mkForce "rDbPq/hH40iA57D4vlFX+1heF4USlGCv24IiQDgOdEg=";
+      };
+    })
   ];
-
-  # Hash fix until nixpkgs gets updated
-  pkgs.bombsquad = pkgs.stdenv.mkDerivation rec {
-    name = "bombsquad-1.7.37";
-    src = pkgs.fetchurl {
-      url = "https://files.ballistica.net/bombsquad/builds/BombSquad_Linux_x86_64_1.7.37.tar.gz";
-      sha256 = "rDbPq/hH40iA57D4vlFX+1heF4USlGCv24IiQDgOdEg=";
-    };
-  };
 
 }
