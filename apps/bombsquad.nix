423bd7bcc98f61ecc36db80af96c8e6a1b7d31d5
tob4n <109690594+tob4n@users.noreply.github.com>
2025-01-07 19:14:16 -0800

    Update bombsquad.nix

--- apps/bombsquad.nix
+++ apps/bombsquad.nix
@@ -8,6 +8,10 @@
         url = "https://files.ballistica.net/bombsquad/builds/BombSquad_Linux_x86_64_1.7.37.tar.gz";
         sha256 = "rDbPq/hH40iA57D4vlFX+1heF4USlGCv24IiQDgOdEg=";
       };
+      installPhase = ''
+        mkdir -p $out
+        cp -r $src/* $out/
+      '';
     })
   ];
 
