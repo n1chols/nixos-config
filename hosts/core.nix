308e7d999581b94ff670e0863b5f5c6a652ae4e0
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 13:58:10 -0800

    Update core.nix

--- hosts/core.nix
+++ hosts/core.nix
@@ -1,28 +1,23 @@
 { config, pkgs, ... }: {
 
-  # Boot
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
 
-  # Network
   networking.networkmanager.enable = true;
 
-  # User
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
   };
 
-  # Packages
   nixpkgs.config.allowUnfree = true;
 
   documentation.nixos.enable = false;
 
   services.xserver.excludePackages = [ pkgs.xterm ];
 
-  # Version
   system.stateVersion = "24.11";
 
 }
