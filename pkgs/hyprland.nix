56c1acdeafe6ffb1dcf5995c5c5e16bd97f0f9b1
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 15:02:38 -0800

    Update hyprland.nix

--- pkgs/hyprland.nix
+++ pkgs/hyprland.nix
@@ -6,4 +6,13 @@
     withUWSM = false;
   };
 
+  # Required environment variables for Wayland
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+  };
+
 }
