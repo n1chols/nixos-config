000c91a2500f0516f54353f23d0e4eddc34691fe
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 00:26:26 -0800

    Update and rename pegasus.nix to pegasus.nix

--- pkgs/pegasus.nix
+++ pkgs/pegasus.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }: {
 
+  # Enable Gamescope
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  
   # Install Pegasus
   environment.systemPackages = with pkgs; [
     pegasus-frontend
