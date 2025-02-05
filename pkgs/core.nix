939ae4c251a274e6537ab74bde67d21f5341ff7a
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 14:09:50 -0800

    Update and rename core.nix to core.nix

--- pkgs/core.nix
+++ pkgs/core.nix
@@ -1,23 +1,29 @@
 { config, pkgs, ... }: {
 
+  # Setup EFI boot loader
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
 
+  # Enable NetworkManager
   networking.networkmanager.enable = true;
 
+  # Create user account with sudo and network access
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
   };
 
+  # Allow proprietary software
   nixpkgs.config.allowUnfree = true;
 
+  # Disable documentation and xterm
   documentation.nixos.enable = false;
 
   services.xserver.excludePackages = [ pkgs.xterm ];
 
+  # NixOS config version
   system.stateVersion = "24.11";
 
 }
