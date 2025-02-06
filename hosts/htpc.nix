e3fa693449c344f661570298f2a26b75bd626752
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 22:56:16 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -58,7 +58,7 @@
       enable = true;
       commands = [
         "${pkgs.gamescope}/bin/gamescope -w 3840 -h 2160 -r 120 --backend drm --immediate-flips --rt --fullscreen --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
-        "dbus-launch --exit-with-session ck-launch-session startplasma-wayland"
+        "dbus-launch --exit-with-session startplasma-wayland"
       ];
     };
   };
