124708920b6e513aa5818cb099f72dab3a09c1b8
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 16:09:49 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -56,7 +56,7 @@
       enable = true;
       commands = [
         "${pkgs.gamescope}/bin/gamescope -w 3840 -h 2160 -r 120 --backend drm --immediate-flips --rt --fullscreen --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
-        "${pkgs.dbus}/bin/dbus-run-session ${pkgs.hyprland}/bin/Hyprland"
+        "${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.hyprland}/bin/Hyprland"
       ];
     };
   };
