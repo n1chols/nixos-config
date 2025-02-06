3526c91f65ea1ea78dda21234bdb57980bc69d61
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-06 00:07:08 -0800

    Update htpc.nix

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -39,7 +39,7 @@
   # IMPORTS / MODULES
   imports = [
     ../pkgs/core.nix
-    ../pkgs/plasma.nix
+    ../pkgs/hyprland.nix
     ../pkgs/pegasus.nix
     ../pkgs/steam.nix
     ../pkgs/kodi.nix
@@ -58,7 +58,7 @@
       enable = true;
       commands = [
         "${pkgs.gamescope}/bin/gamescope -w 3840 -h 2160 -r 120 --backend drm --immediate-flips --rt --fullscreen --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
-        "dbus-launch env WAYLAND_DISPLAY=wayland-1 --exit-with-session startplasma-wayland"
+        "${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.hyprland}/bin/Hyprland"
       ];
     };
   };
