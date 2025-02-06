9a41cc00be4c792abbc3c79e2dea498e3839791d
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 16:02:16 -0800

    Update update.nix

--- modules/update.nix
+++ modules/update.nix
@@ -17,7 +17,7 @@
         #!${pkgs.bash}/bin/bash
         set -e
         cd /etc/nixos
-        sudo rm -rf *
+        sudo find . -mindepth 1 -delete
         sudo git clone ${config.modules.update.repo} .
         if [ -n "$1" ]; then
           sudo nixos-rebuild switch --flake .#$1
