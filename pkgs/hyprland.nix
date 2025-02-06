370b8ec6f04d6cfb9ba7092da493501bc8ab17e5
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-05 18:12:41 -0800

    Update hyprland.nix

--- pkgs/hyprland.nix
+++ pkgs/hyprland.nix
@@ -4,27 +4,6 @@
   programs.hyprland = {
     enable = true;
     withUWSM = false;
-    settings = {
-      monitor = [
-        ",preferred,auto,1"
-      ];
-        
-      exec-once = [
-        "waybar"
-        "dunst"
-      ];
-        
-      bind = [
-        "SUPER, Q, exec, rofi --show drun"
-        "SUPER, T, exec, kitty"
-        "SUPER, F, fullscreen"
-      ];
-
-      input = {
-        kb_layout = "us";
-        follow_mouse = 1;
-      };
-    };
   };
 
   # Install desktop utilities
