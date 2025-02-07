78b7dbf520ef79547fc0d9cbe68009df51703a5b
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-06 22:36:54 -0800

    Update and rename update.nix to update-command.nix

---
+++ utils/update-command.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }: {
+
+  # Install git and update command
+  environment.systemPackages = with pkgs; [
+    git
+    (pkgs.writeShellScriptBin "update" ''
+      #!${pkgs.bash}/bin/bash
+      set -e
+      cd /etc/nixos
+      sudo find . -mindepth 1 -delete
+      sudo git clone https://github.com/tob4n/nixos-config .
+      if [ -n "$1" ]; then
+        sudo nixos-rebuild switch --flake .#$1 --no-write-lock-file
+      else
+        sudo nixos-rebuild switch --flake . --no-write-lock-file
+      fi
+    '')
+  ];
+
+}
