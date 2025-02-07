09c9dac8d034ee4bc4d7d110085141fb1c85825a
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-06 23:18:44 -0800

    Update gamescope.nix

--- pkgs/gamescope.nix
+++ pkgs/gamescope.nix
@@ -3,7 +3,19 @@
   # Enable Gamescope
   programs.gamescope = {
     enable = true;
-    capSysNice = true;
+    args = {
+      "--backend drm"
+      "--expose-wayland"
+      "--fullscreen"
+    };
+  };
+
+  # Set capabilities for gamescope
+  security.wrappers.gamescope = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_nice+eip";
+    source = "${pkgs.gamescope}/bin/gamescope";
   };
 
 }
