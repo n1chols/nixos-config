721bbc4f81eb4377a4881441ce2d4053111b49c1
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-06 21:24:45 -0800

    Update pegasus.nix

--- pkgs/pegasus.nix
+++ pkgs/pegasus.nix
@@ -1,16 +1,4 @@
 { config, lib, pkgs, ... }: {
-
-  # Enable Gamescope
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-    args = [
-      "--backend drm"
-      "--immediate-flips"
-      "--rt"
-      "--fullscreen"
-    ];
-  };
   
   # Install Pegasus
   environment.systemPackages = with pkgs; [
