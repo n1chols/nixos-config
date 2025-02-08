c7725cd400166f6dd39480dafbaf4221664de63a
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-08 00:16:06 -0800

    Update opengamepadui.nix

--- pkgs/opengamepadui.nix
+++ pkgs/opengamepadui.nix
@@ -3,19 +3,22 @@
   # Enable OpenGamepadUI
   programs.opengamepadui = {
     enable = true;
-    gamescopeSession.args = [
-      "--backend drm"
-      "--expose-wayland"
-      "--fullscreen"
-      "--immediate-flips"
-      "--rt"
-      "-W 3840"
-      "-H 2160"
-      "-r 120"
-      "--adaptive-sync"
-      "--hdr-enabled"
-      "--hdr-itm-enable"
-    ];
+    gamescopeSession = {
+      enable = true;
+      args = [
+        "--backend drm"
+        "--expose-wayland"
+        "--fullscreen"
+        "--immediate-flips"
+        "--rt"
+        "-W 3840"
+        "-H 2160"
+        "-r 120"
+        "--adaptive-sync"
+        "--hdr-enabled"
+        "--hdr-itm-enable"
+      ];
+    };
   };
 
 }
