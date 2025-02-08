b2fc044cef3058f02446a2460292d05a640406c5
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-07 23:47:28 -0800

    Update default.nix

--- hosts/htpc/default.nix
+++ hosts/htpc/default.nix
@@ -64,12 +64,9 @@
       enable = true;
       commands = [
         "${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.hyprland}/bin/Hyprland"
-        "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 -r 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- ${pkgs.pegasus-frontend}/bin/pegasus-fe"
+        "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 -r 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
       ];
     };
   };
-
-  #"${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.hyprland}/bin/Hyprland"
-  #"${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 -r 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent & ${pkgs.pegasus-frontend}/bin/pegasus-fe'"
 
 }
