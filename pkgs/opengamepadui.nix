4a353864ce943136c4e7b4c11077cff11d01afb2
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-08 00:04:25 -0800

    Create opengamepadui.nix

---
+++ pkgs/opengamepadui.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }: {
+
+  # Enable OpenGamepadUI
+  programs.opengampadui = {
+    enable = true;
+    gamescopeSession.Args = [
+      "--backend drm"
+      "--expose-wayland"
+      "--fullscreen"
+      "--immediate-flips"
+      "--rt"
+      "-W 3840"
+      "-H 2160"
+      "-r 120"
+      "--adaptive-sync"
+      "--hdr-enabled"
+      "--hdr-itm-enable"
+    ];
+  }
+
+}
