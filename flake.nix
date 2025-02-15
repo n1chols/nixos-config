d37407d1bcd4f428fd93c2f7bb372fd9a50c72af
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 00:55:49 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     simple-system.url = "github:tob4n/nixos-simple-system";
   };
   outputs = { nixpkgs, simple-system, ... }: {
-    nixosConfigurations.htpc = (simple-system.mkSystem) {
+    nixosConfigurations.htpc = simple-system.mkSystem {
       hostName = "htpc";
       userName = "user";
 
@@ -19,21 +19,21 @@
       hiResAudio = true;
 
       extraModules = [
-        #./modules/gamescope.nix
-        #./modules/steam.nix
-        #./modules/kodi.nix
-        #./modules/roon-server.nix
-        #./modules/update-command.nix
-        #({ pkgs, ... }: {
-        #  environment.systemPackages = [ pkgs.pegasus-frontend ];
-        #  services.greetd = {
-        #    enable = true;
-        #    settings.default_session = {
-        #      user = "user";
-        #      command = "gamescope -W 3840 -H 2160 -r 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c 'steam -silent -nofriendsui & pegasus-fe'";
-        #    };
-        #  };
-        #})
+        ./modules/gamescope.nix
+        ./modules/steam.nix
+        ./modules/kodi.nix
+        ./modules/roon-server.nix
+        ./modules/update-command.nix
+        ({ pkgs, ... }: {
+          environment.systemPackages = [ pkgs.pegasus-frontend ];
+          services.greetd = {
+            enable = true;
+            settings.default_session = {
+              user = "user";
+              command = "gamescope -W 3840 -H 2160 -r 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c 'steam -silent -nofriendsui & pegasus-fe'";
+            };
+          };
+        })
       ];
     };
   };
