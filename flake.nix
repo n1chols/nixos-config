a26d79dc510179b718190ef922fd958df8b85702
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 20:59:14 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -34,13 +34,13 @@
             cemu
             ryujinx
           ];
-          #services.greetd = {
-          #  enable = true;
-          #  settings.default_session = {
-          #    user = "user";
-          #    command = "gamescope -W 3840 -H 2160 --framerate-limit 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c 'steam -silent -nofriendsui & pegasus-fe' > /dev/null 2>&1";
-          #  };
-          #};
+          services.greetd = {
+            enable = true;
+            settings.default_session = {
+              user = "user";
+              command = "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 --framerate-limit 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c 'env DXVK_HDR=1 ${pkgs.steam}/bin/steam -silent -nofriendsui & ${pkgs.pegasus-frontend}/bin/pegasus-fe' > /dev/null 2>&1";
+            };
+          };
         })
       ];
     };
