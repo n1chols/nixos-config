cd703eb03da48dda3cecc2977a285138c667a049
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 21:27:46 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -38,7 +38,8 @@
             enable = true;
             settings.default_session = {
               user = "user";
-              command = "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 --framerate-limit 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent -nofriendsui & ${pkgs.pegasus-frontend}/bin/pegasus-fe' > /dev/null 2>&1";
+              command = "${pkgs.dbus}/bin/dbus-run-session env XDG_SESSION_TYPE=wayland ${pkgs.gnome-session}/bin/gnome-session"
+              #command = "${pkgs.gamescope}/bin/gamescope -W 3840 -H 2160 --framerate-limit 120 --adaptive-sync --hdr-enabled --hdr-itm-enable -- sh -c '${pkgs.steam}/bin/steam -silent -nofriendsui & ${pkgs.pegasus-frontend}/bin/pegasus-fe' > /dev/null 2>&1";
             };
           };
         })
