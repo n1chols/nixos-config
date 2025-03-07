f2f72c97958ba41019a198ad34e29f21efeafed5
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-06 22:29:13 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,17 +21,17 @@
       gamepad = true;
 
       extraModules = [
-        ./modules/gamescope.nix
         ./modules/steam.nix
         ./modules/kodi.nix
         ./modules/roon-server.nix
+        ./modules/gamescope-rt.nix
         ./modules/update-command.nix
         ({ pkgs, ... }: {
           services.greetd = {
             enable = true;
             settings.default_session = {
               user = "user";
-              command = "${pkgs.gamescope}/bin/gamescope -f -e --adaptive-sync --hdr-enabled --hdr-itm-enable -- ${pkgs.steam}/bin/steam -gamepadui -pipewire-dmabuf";# > /dev/null 2>&1";
+              command = "${pkgs.gamescope-rt}/bin/gamescope-rt -f -e --adaptive-sync --hdr-enabled --hdr-itm-enable -- ${pkgs.steam}/bin/steam -gamepadui -pipewire-dmabuf";# > /dev/null 2>&1";
             };
           };
         })
