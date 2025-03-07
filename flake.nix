6a0bf96049d703c83c69fa255ef050bc5e88298a
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-06 20:33:54 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   outputs = { nixpkgs, simple-system, ... }: {
-    nixosConfigurations.htpc = simple-system.mkSystem {
+    nixosConfigurations.htpc = simple-system {
       hostName = "htpc";
       userName = "user";
 
