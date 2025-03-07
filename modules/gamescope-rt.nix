d9ba45e16d4775bea76d80945b831f492689ec21
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-06 23:44:50 -0800

    Update gamescope-rt.nix

--- modules/gamescope-rt.nix
+++ modules/gamescope-rt.nix
@@ -1,17 +1,15 @@
 { pkgs, ... }: {
-  gamescope-rt-base = pkgs.runCommand "gamescope-rt-base" {
-    nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
-  } ''
-    mkdir -p $out/bin
-    cp ${pkgs.gamescope}/bin/gamescope $out/bin/gamescope-rt
-    wrapProgram $out/bin/gamescope-rt --add-flags "--rt --immediate-flips --backend drm"
-  '';
-
   security.wrappers.gamescope-rt = {
     owner = "root";
     group = "root";
     capabilities = "cap_sys_nice+eip";
-    source = "${pkgs.gamescope-rt-base}/bin/gamescope-rt";
+    source = "${pkgs.runCommand "gamescope-rt" {
+      nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+    } ''
+      mkdir -p $out/bin
+      cp ${pkgs.gamescope}/bin/gamescope $out/bin/gamescope-rt
+      wrapProgram $out/bin/gamescope-rt --add-flags "--rt --immediate-flips"
+    ''}/bin/gamescope-rt";
   };
 
   environment.systemPackages = [
