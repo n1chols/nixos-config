1e9ca84c6ea60c4346ad519fe0a6660d04b427cc
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-12 06:06:27 -0700

    Update bombsquad.nix

--- modules/bombsquad.nix
+++ modules/bombsquad.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     (pkgs.bombsquad.overrideAttrs (oldAttrs: rec {
       src = pkgs.fetchurl {
-        url = "https://files.ballistica.net/bombsquad/builds/BombSquad_Linux_x86_64_1.7.37.tar.gz";
+        url = "https://files.ballistica.net/bombsquad/builds/old/BombSquad_Linux_x86_64_1.7.37.tar.gz";
         sha256 = "rDbPq/hH40iA57D4vlFX+1heF4USlGCv24IiQDgOdEg=";
       };
     }))
