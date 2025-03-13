0e4d6e94738379af432ceb399fb610680d4f418c
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-13 05:01:16 -0700

    Update plasma-bigscreen.nix

--- modules/plasma-bigscreen.nix
+++ modules/plasma-bigscreen.nix
@@ -6,14 +6,14 @@
     useQtScaling = true;
   };
 
-  environment.systemPackages = [
-    pkgs.writeScriptBin "plasma-bigscreen" ''
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeScriptBin "plasma-bigscreen" ''
       #!/bin/sh
       export QT_QPA_PLATFORM=wayland
       ${pkgs.dbus}/bin/dbus-run-session \
       ${pkgs.plasma5Packages.kwin}/bin/kwin_wayland \
       "${pkgs.plasma5Packages.plasma-workspace}/bin/plasmashell -p org.kde.plasma.bigscreen"
-    '';
+    '')
   ];
 
 }
