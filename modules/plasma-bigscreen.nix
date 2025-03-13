a5f8473e508436e642f8b5b4cce1e2e0046ef1b8
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-13 05:09:38 -0700

    Update plasma-bigscreen.nix

--- modules/plasma-bigscreen.nix
+++ modules/plasma-bigscreen.nix
@@ -1,18 +1,16 @@
 { pkgs, ... }: {
-  
-  services.xserver.desktopManager.plasma5 = {
-    enable = true;
-    bigscreen.enable = true;
-    useQtScaling = true;
-  };
+
+  services.desktopManager.plasma6.enable
+
+  services.xserver.desktopManager.plasma5.bigscreen.enable = true;
 
   environment.systemPackages = with pkgs; [
     (pkgs.writeScriptBin "plasma-bigscreen" ''
       #!/bin/sh
       export QT_QPA_PLATFORM=wayland
       ${pkgs.dbus}/bin/dbus-run-session \
-      ${pkgs.plasma5Packages.kwin}/bin/kwin_wayland \
-      "${pkgs.plasma5Packages.plasma-workspace}/bin/plasmashell -p org.kde.plasma.bigscreen"
+      ${pkgs.kdePackages.kwin}/bin/kwin_wayland \
+      "${pkgs.kdePackages.plasma-workspace}/bin/plasmashell -p org.kde.plasma.bigscreen"
     '')
   ];
 
