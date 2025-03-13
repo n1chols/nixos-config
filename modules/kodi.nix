6cb90ade22e12206c3a88223c840ca22ea56d1f8
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-13 03:17:17 -0700

    Update kodi.nix

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -1,16 +1,11 @@
 { config, lib, pkgs, ... }: {
 
   # Install Kodi
-  #environment.systemPackages = with pkgs; [
-  #  (kodi.withPackages (pkgs: with pkgs; [
-  #    joystick
-  #    inputstream-adaptive
-  #  ]))
-  #];
-
-  services.xserver.desktopManager.kodi.package = (pkgs.kodi.withPackages (kodiPkgs: with kodiPkgs; [
-    joystick
-    inputstream-adaptive
-  ]));
+  environment.systemPackages = with pkgs; [
+    (kodi.withPackages (pkgs: with pkgs; [
+      joystick
+      inputstream-adaptive
+    ]))
+  ];
 
 }
