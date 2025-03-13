03cba17314fc94681e1c55720649309f226f879a
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-13 06:27:35 -0700

    Update plasma-bigscreen.nix

--- modules/plasma-bigscreen.nix
+++ modules/plasma-bigscreen.nix
@@ -2,15 +2,27 @@
 
   boot.kernelPackages = pkgs.linuxPackages;
 
+  services.xserver.enable = true;
+
   services.desktopManager.plasma6.enable = true;
+
+  programs.dconf.enable = true;
+  
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
 
   environment.systemPackages = with pkgs; [
     (pkgs.writeScriptBin "plasma-bigscreen" ''
       #!/bin/sh
       export QT_QPA_PLATFORM=wayland
-      ${pkgs.dbus}/bin/dbus-run-session \
-      ${pkgs.kdePackages.kwin}/bin/kwin_wayland \
-      "${pkgs.kdePackages.plasma-workspace}/bin/plasmashell" #-p org.kde.plasma.bigscreen"
+      startplasma-wayland
+      # test
+      #${pkgs.dbus}/bin/dbus-run-session \
+      #${pkgs.kdePackages.kwin}/bin/kwin_wayland \
+      #"${pkgs.kdePackages.plasma-workspace}/bin/plasmashell" #-p org.kde.plasma.bigscreen"
     '')
   ];
 
