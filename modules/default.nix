41480828a8c2457b8094709200fb9f20b89135d9
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 16:28:49 -0700

    Create default.nix

---
+++ modules/default.nix
@@ -0,0 +1,14 @@
+{
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Remove default packages
+  environment.defaultPackages = [];
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable all firmware
+  hardware.enableAllFirmware = true;
+}
