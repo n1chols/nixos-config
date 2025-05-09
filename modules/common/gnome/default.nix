7f5fb2e4668174e3b52000d54e6dce67a234864c
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 23:02:54 -0700

    Create default.nix

---
+++ modules/common/gnome/default.nix
@@ -0,0 +1,25 @@
+{
+  # Enable GNOME
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  # Enable XDG desktop portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
+  };
+
+  # Disable GNOME core apps and setup
+  services.gnome = {
+    core-utilities.enable = false;
+    gnome-initial-setup.enable = false;
+  };
+
+  # Disable the remaining apps
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+    gnome-shell-extensions
+  ]);
+}
