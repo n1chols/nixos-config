2005c1eba01cb66d0ef8af6f91cab746f6b8d9aa
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 22:38:48 -0700

    Update default.nix

--- modules/host/htpc/default.nix
+++ modules/host/htpc/default.nix
@@ -1,1 +1,17 @@
+{ 
+  fileSystem = {
+    "/boot".device = "/dev/nvme0n1p1";
+    "/".device = "/dev/nvme0n1p2";
+    swapDevices = [{ device = "/dev/nvme0n1p3"; }]
+  };
 
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    initrd.kernelModules = [ "amdgpu" ];
+  };
+
+  hardware = {
+    cpu.amd.updateMicrocode = true;
+    xpadneo.enable = true;
+  };
+}
