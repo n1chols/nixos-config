e73edcd8b1eacba6d3befd00aeeb5d787992fede
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 22:30:41 -0700

    Create default.nix

---
+++ modules/common/pipewire/default.nix
@@ -0,0 +1,12 @@
+{
+  # Enable RTKit
+  security.rtkit.enable = true;
+
+  # Enable PipeWire
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+}
