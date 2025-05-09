81f9e112965b9aef588f95f93a8582226fec928b
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 22:42:50 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,29 +10,13 @@
       version = "24.11";
       
       modules = [
+        ./modules/common/networkmanager
+        ./modules/common/pipewire
+        ./modules/common/bluetooth
+        ./modules/host/htpc
+        ./modules/user/user
         steam-console.modules.default
         { ... }: {
-          fileSystem = {
-            "/boot".device = "/dev/nvme0n1p1";
-            "/".device = "/dev/nvme0n1p2";
-            swapDevices = [{ device = "/dev/nvme0n1p3"; }]
-          };
-
-          boot = {
-            kernelPackages = pkgs.linuxPackages_zen;
-            initrd.kernelModules = [ "amdgpu" ];
-          };
-
-          hardware = {
-            cpu.amd.updateMicrocode = true;
-            xpadneo.enable = true;
-          };
-
-          users.users.user = {
-            isNormalUser = true;
-            extraGroups = [ "networkmanager" "wheel" ];
-          };
-
           services = {
             roon-server = {
               enable = true;
