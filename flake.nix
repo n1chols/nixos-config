8f5f56e01dfda5b2803a7492dc9d2c6949fa667c
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-08 22:49:23 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,36 +5,51 @@
   };
 
   outputs = { flex-system, steam-console }: {
-    nixosConfigurations.htpc = flex-system {
-      arch = "x86_64";
-      version = "24.11";
-      
-      modules = [
-        ./modules/common/networkmanager
-        ./modules/common/pipewire
-        ./modules/common/bluetooth
-        ./modules/host/htpc
-        ./modules/user/user
-        steam-console.modules.default
-        { ... }: {
-          services = {
-            roon-server = {
-              enable = true;
-              openFirewall = true;
+    nixosConfigurations = {
+      htpc = flex-system {
+        arch = "x86_64";
+        version = "24.11";
+        
+        modules = [
+          ./modules/common/networkmanager
+          ./modules/common/pipewire
+          ./modules/common/bluetooth
+          ./modules/host/htpc
+          ./modules/user/user
+          steam-console.modules.default
+          { ... }: {
+            services = {
+              roon-server = {
+                enable = true;
+                openFirewall = true;
+              };
+              desktopManager.plasma6.enable = true;
             };
-            desktopManager.plasma6.enable = true;
-          };
-
-          steam-console = {
-            enable = true;
-            enableHDR = true;
-            enableVRR = true;
-            enableDecky = true;
-            user = "user";
-            desktopSession = "startplasma-wayland";
+  
+            steam-console = {
+              enable = true;
+              enableHDR = true;
+              enableVRR = true;
+              enableDecky = true;
+              user = "user";
+              desktopSession = "startplasma-wayland";
+            }
           }
-        }
-      ];
+        ];
+      };
+      thinkpad = flex-system {
+        arch = "x86_64";
+        version = "24.11";
+        
+        modules = [
+          ./modules/common/networkmanager
+          ./modules/common/pipewire
+          ./modules/common/bluetooth
+          ./modules/common/printing
+          ./modules/host/thinkpad
+          ./modules/user/user
+        ]
+      };
     };
   };
 }
